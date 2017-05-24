c4ee90fc9c8adccd4e50e53a8334654c2c3ebfc0
Joey <jjowcl01@gmail.com>
2017-05-24 23:40:35 +0800

    first commt

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+/*
+   Sticky Nav
+   ========================================================================== */
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $('.header-top-area').addClass('menu-bg');
+        } else {
+            $('.header-top-area').removeClass('menu-bg');
+        }
+    });
+
+/* 
+   VIDEO POP-UP
+   ========================================================================== */
+    $('.video-popup').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+    });
+
+/* 
+   Back Top Link
+   ========================================================================== */
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
+    $('.back-to-top').click(function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    })
+
+/* 
+   One Page Navigation & wow js
+   ========================================================================== */
+  jQuery(function($) {
+      //Initiat WOW JS
+      new WOW().init();
+
+  });
+
+  jQuery(document).ready(function() {
+     
+      $('body').scrollspy({
+          target: '.navbar-collapse',
+          offset: 195
+      });
+
+      $(window).on('scroll', function() {
+          if ($(window).scrollTop() > 200) {
+              $('.fixed-top').addClass('menu-bg');
+          } else {
+              $('.fixed-top').removeClass('menu-bg');
+          }
+      });
+
+  });
+
+/* 
+   Page Loader
+   ========================================================================== */
+   $(window).load(function() {
+    "use strict";
+    $('#loader').fadeOut();
+   });
